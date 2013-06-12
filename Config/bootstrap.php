db87966875d9bfd8b70c9bf016011f5350371c87
Juraj Jancuska <jjancuska@gmail.com>
2013-06-12 12:04:17 +0200

    Load config menu in bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,14 +1,14 @@
 <?php
-/**
- * Helper
- *
- * This plugin's Texy helper will be loaded via NodesController.
- */
         Croogo::hookHelper('Nodes', 'Texy.Texy');
-/**
- * Admin menu (navigation)
- *
- * This plugin's admin_menu element will be rendered in admin panel under Extensions menu.
- */
-        Croogo::hookAdminMenu('Texy');
+
+        CroogoNav::add('extensions.children.texy', array(
+            'title' => 'Texy',
+            'url' => '#',
+            'children' => array(
+                'settings' => array(
+                    'title' => __('Configure'),
+                    'url' => array('plugin' => '', 'controller' => 'settings', 'action' => 'prefix', 'Texy')
+                )
+            )
+        ));        
 ?>
